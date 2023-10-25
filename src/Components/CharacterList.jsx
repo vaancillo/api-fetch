d68a3f5eda55849949ebd269d1b881d848d46c6e
vancillo <yaredbane@gmail.com>
2023-10-25 12:33:58 -0600

    finish the project

--- src/Components/CharacterList.jsx
+++ src/Components/CharacterList.jsx
@@ -2,24 +2,40 @@ import { useEffect, useState } from 'react'
 import Character from './Character'
 import Spinner from './Spinner'
 
+function NavPage ({ page, setPage }) {
+  return (
+    <header className='d-flex justify-content-between align-items-center'>
+      <p>Page: {page}</p>
+      <button
+        className='btn btn-primary btn-sm'
+        onClick={() => setPage(page + 1)}
+      >
+        Page: {page + 1}
+      </button>
+    </header>
+  )
+}
+
 const CharacterList = () => {
   const [characters, setCharacters] = useState([])
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
     async function fetchData () {
-      const res = await fetch('https://rickandmortyapi.com/api/character')
+      const res = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
       const data = await res.json()
       setLoading(false)
       setCharacters(data.results)
     }
     fetchData()
-  }, [])
+  }, [page])
   // if (loading) {
   //   return <Spinner />
   // }
   return (
     <div className='container'>
+      <NavPage page={page} setPage={setPage} />
       {
         loading
           ? (
@@ -27,10 +43,10 @@ const CharacterList = () => {
             )
           : (
 
-            <div className='row'>
+            <div className='row row-cols-4 gap-3 justify-content-center'>
               {characters.map((character) => {
                 return (
-                  <div key={character.id} className='col-md-4'>
+                  <div key={character.id} className='col'>
                     <Character character={character} />
                   </div>
                 )
@@ -38,7 +54,7 @@ const CharacterList = () => {
             </div>
 
             )
-}
+}             <NavPage page={page} setPage={setPage} />
     </div>
   )
 }
